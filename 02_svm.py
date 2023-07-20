ef48cf0eaba5a8703b3bd2fa34593a75149cdc0d
DiegoLigtenberg <diegoligtenberg@gmail.com>
2023-07-21 00:04:37 +0200

    added all model files with working code
    logistic regression
    svm
    feed forward nn
    convolutional neural network
    vision transformer (06, 05 not working),
    
    these files serve as example, but many paths lead to Rome

---
+++ 02_svm.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import warnings
+
+# Suppress DeprecationWarnings for the entire NumPy module
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="numpy")
+
+# Load the kaggle digit dataset
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+# Convert the DataFrame to NumPy arrays with dtype=np.float32
+X = train.drop(labels=["label"], axis=1).values.astype(np.float32)
+Y = train["label"].values.astype(np.float32)
+
+X = X[:500]
+Y = Y[:500]
+
+X_test = test.values.astype(np.float32)
+
+# Step 1: Split the data into training and validation sets
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# Step 2: Create an SVM model
+svm_model = SVC()
+
+# Step 3: Fit the model on the training data
+svm_model.fit(X_train, Y_train)
+
+# Optionally, you can check the training accuracy
+train_predictions = svm_model.predict(X_train)
+train_accuracy = accuracy_score(Y_train, train_predictions)
+print("Training accuracy:", train_accuracy)
+
+# Step 4: Make predictions on the validation data
+val_predictions = svm_model.predict(X_val)
+
+# Step 5: Calculate the validation accuracy
+val_accuracy = accuracy_score(Y_val, val_predictions)
+print("Validation accuracy:", val_accuracy)
+
+# Step 6: Make predictions on the X_test data
+test_predictions = svm_model.predict(X_test)
+
+# Visualize the predictions on X_test
+num_samples_to_visualize = 10
+sample_indices = np.random.choice(X_test.shape[0], num_samples_to_visualize, replace=False)
+
+plt.figure(figsize=(12, 6))
+for i, index in enumerate(sample_indices):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(X_test[index].reshape(28, 28), cmap='gray')
+    predicted_label = int(test_predictions[index])
+    plt.title(f"Predicted Label: {predicted_label}")
+    plt.axis('off')
+
+plt.show()
---
+++ 03_feedforward_nn.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers
+
+# Load the kaggle digit dataset
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+# Convert the DataFrame to NumPy arrays with dtype=np.float32
+X = train.drop(labels=["label"], axis=1).values.astype(np.float32)
+Y = train["label"].values.astype(np.int32)
+
+X_test = test.values.astype(np.float32)
+
+# Step 1: Split the data into training and validation sets
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# Step 2: Create a simple feedforward neural network model
+model = tf.keras.Sequential([
+    layers.Dense(128, activation='relu', input_shape=(784,)),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(10, activation='softmax')
+])
+
+# Step 3: Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Step 4: Fit the model on the training data
+history = model.fit(X_train, Y_train, epochs=20, batch_size=32, validation_data=(X_val, Y_val))
+
+# Optionally, you can check the training and validation accuracy
+train_predictions = model.predict_classes(X_train)
+train_accuracy = accuracy_score(Y_train, train_predictions)
+print("Training accuracy:", train_accuracy)
+
+val_predictions = model.predict_classes(X_val)
+val_accuracy = accuracy_score(Y_val, val_predictions)
+print("Validation accuracy:", val_accuracy)
+
+# Step 5: Make predictions on the X_test data
+test_predictions = model.predict_classes(X_test)
+
+# Visualize the predictions on X_test
+num_samples_to_visualize = 10
+sample_indices = np.random.choice(X_test.shape[0], num_samples_to_visualize, replace=False)
+
+plt.figure(figsize=(12, 6))
+for i, index in enumerate(sample_indices):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(X_test[index].reshape(28, 28), cmap='gray')
+    predicted_label = int(test_predictions[index])
+    plt.title(f"Predicted Label: {predicted_label}")
+    plt.axis('off')
+
+plt.show()
